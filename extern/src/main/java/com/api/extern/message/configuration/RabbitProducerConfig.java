822f6fa48b5167784ab72d1ed965f2daa6c37dd4
MFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2024-02-05 16:09:58 -0400

    Update Preliminar
    Implementando de fora beta sistema de mensageria

---
+++ extern/src/main/java/com/api/extern/message/configuration/RabbitProducerConfig.java
@@ -0,0 +1,10 @@
+package com.api.extern.message.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitProducerConfig {
+    @Bean
+    public 
+}

bc1e23e6eddc81ee29787f30fb728aa8f896e8ca
MFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2024-03-01 10:48:15 -0400

    Update RabbitProducerConfig.java
    Este é inutil

--- extern/src/main/java/com/api/extern/message/configuration/RabbitProducerConfig.java
+++ extern/src/main/java/com/api/extern/message/configuration/RabbitProducerConfig.java
@@ -1,10 +1,1 @@
-package com.api.extern.message.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RabbitProducerConfig {
-    @Bean
-    public 
-}
